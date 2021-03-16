3fb29b9e9c6ad34b4e627d772c7a34a2ba52d3f3
poximy <zv.erick@protonmail.com>
2021-03-16 12:50:53 -0600

    feat(components): Create Url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
+import Url from "./components/Url";
 
 const App: React.FC = () => {
   return (
     <>
+      <Url />
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/Url.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Url: React.FC = () => {
+  return (
+      <form>
+          <input type="text"/>
+          <input type="submit" value="Minify Url"/>
+      </form>
+  )
+};
+
+export default Url;
