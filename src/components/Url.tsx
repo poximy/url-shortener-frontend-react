4f87efdf45280ca98b3fdf970334c9fc135e290d
poximy <zv.erick@protonmail.com>
2021-03-16 13:34:12 -0600

    feat(Url): Create onSubmit action

--- src/components/Url.tsx
+++ src/components/Url.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Url: React.FC = () => {
+  const [urlText, setUrlText] = useState<string>("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!urlText) {
+      alert("Insert Text!");
+      return;
+    }
+
+    setUrlText("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         value={urlText}
