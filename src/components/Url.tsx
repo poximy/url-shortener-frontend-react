34b61c792f63734933a79a3e5be104ea03905d64
poximy <zv.erick@protonmail.com>
2021-03-16 13:30:46 -0600

    feat(Url): Create form

--- src/components/Url.tsx
+++ src/components/Url.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const Url: React.FC = () => {
   return (
-      <form>
-          <input type="text"/>
-          <input type="submit" value="Minify Url"/>
-      </form>
-  )
+    <form>
+      <input
+        type="text"
+        value={urlText}
+        placeholder="Url"
+        onChange={(e) => setUrlText(e.target.value)}
+      />
+      <input type="submit" value="Minify Url" />
+    </form>
+  );
 };
 
 export default Url;
