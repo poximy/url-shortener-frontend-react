202aa8f04e57a3cfee9607032292d385d1778487
poximy <zv.erick@protonmail.com>
2021-03-17 13:34:26 -0600

    feat(Url): Add UrlForm

--- src/components/Url.tsx
+++ src/components/Url.tsx
@@ -1,53 +1,14 @@
 import React, { useState } from "react";
-
-interface IUrl {
-  _id?: string;
-  urlId?: string;
-  url: string;
-}
+import UrlForm from "./UrlForm";
 
 const Url: React.FC = () => {
-  const [urlText, setUrlText] = useState<string>("");
-
-  // Send a request and gets back a IUrl with complete data
-  const postUrl = async () => {
-    const postData: IUrl = { url: urlText };
-    const url = "http://127.0.0.1:8000/";
-
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(postData),
-    });
-
-    const data: IUrl = await res.json();
-    data.urlId = url + data._id;
-    return data;
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!urlText) {
-      alert("Insert Text!");
-      return;
-    }
-
-    setUrlText("");
-  };
+  const [urlCreated, setUrlCreated] = useState<string>("");
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        value={urlText}
-        placeholder="Url"
-        onChange={(e) => setUrlText(e.target.value)}
-      />
-      <input type="submit" value="Minify Url" />
-    </form>
+    <main>
+      <UrlForm submitAction={(urlData) => setUrlCreated(urlData)} />
+      {urlCreated ? <p>{urlCreated}</p> : ""}
+    </main>
   );
 };
 
